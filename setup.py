6769577009c701a755a0f82b8ec8b688ad874da9
Javier Gonzalez <javierggt@yahoo.com>
2019-12-17 12:57:36 -0500

    Added changes to use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
-from Ska.ParseCM import __version__
-
 setup(name='Ska.ParseCM',
       author = 'CXC Aspect',
       description='Parse MP command management files',
       maintainer_email = 'aldcroft@head.cfa.harvard.edu',
       py_modules = ['Ska.ParseCM'],
-      version=__version__,
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       test_suite='test',
       zip_safe=False,
       packages=['Ska'],
