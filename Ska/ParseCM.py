849298c2c4116691ca9f88722145568d3520541d
Javier Gonzalez <javierggt@yahoo.com>
2020-01-09 15:16:30 -0500

    Use __package__ also in cases where module is not defined in __init__.py

--- Ska/ParseCM.py
+++ Ska/ParseCM.py
@@ -4,7 +4,7 @@ from six.moves import filter
 
 import ska_helpers
 
-__version__ = ska_helpers.get_version('Ska.ParseCM')
+__version__ = ska_helpers.get_version(__package__)
 
 
 def _coerce_type(val):
